6d4b899a8154352f7f93174016241f1e2695d763
harryhritik1999@gmail.com <hritikpankaj1999@gmail.com>
2024-07-25 13:54:37 +0530

    clone

---
+++ lib/domain/usecases/auth/signup.dart
@@ -0,0 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:spotify_clone/core/usecase/usecase.dart';
+import 'package:spotify_clone/data/models/auth/create_user_req.dart';
+import 'package:spotify_clone/domain/repository/auth/auth.dart';
+
+import '../../../service_locator.dart';
+
+class SignupUseCase implements UseCase<Either,CreateUserReq> {
+
+
+  @override
+  Future<Either> call({CreateUserReq ? params}) async {
+    return sl<AuthRepository>().signup(params!);
+  }
+
+}
